9494d93beeab0c175b2ab16363700c58df86c39c
grvnt <109231551+grvnt@users.noreply.github.com>
2024-05-16 05:37:01 +0200

    First working version

--- app/page.js
+++ app/page.js
@@ -44,12 +44,9 @@ export default function Home() {
   };
 
   return (
-    <div className="container max-w-2xl mx-auto p-5">
+    <div className="container max-w-md mx-auto p-5">
       <h1 className="py-6 text-center font-bold text-2xl">
-        Let Your Imagination Wild with{" "}
-        <a href="https://replicate.com/stability-ai/sdxl?utm_source=project&utm_project=getting-started">
-          SDXL
-        </a>
+        Let Your Imagination Wild
       </h1>
 
       <form className="w-full flex" onSubmit={handleSubmit}>
